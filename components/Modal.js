7012a447a519cc9e40d9664de7764a6d4ca7df8f
Amandeep2230 <bhalla.aman99@gmail.com>
2023-03-21 19:33:59 -0400

    posts complete

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,16 @@
-import { useRecoilState } from "recoil";
+import { snapshot, useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom"
 import { Dialog, Transition } from "@headlessui/react"
 import {Fragment, useRef, useState} from "react"
 import { CameraIcon } from "@heroicons/react/outline";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "@firebase/firestore"
+import { useSession} from "next-auth/react"
+import { ref, getDownloadURL, uploadString } from "@firebase/storage"
+import { db, storage } from "../firebase"
+
 function Modal() {
 
+    const { data:session } = useSession()
     const [open, setOpen] = useRecoilState(modalState)
     const filePickerRef = useRef(null)
     const captionRef = useRef(null)
@@ -15,7 +21,34 @@ function Modal() {
         if (loading) return;
 
         setLoading(true)
+        //create post and add to firestore 'posts'
 
+        //get post Id fro created post
+
+        //upload image to firebase storage with the post ID
+
+        //get a download URL from firebase storage and update to original post
+
+        const docRef = await addDoc(collection(db, 'posts'), {
+            username: session.user.name,
+            caption: captionRef.current.value,
+            profileImg: session.user.image,
+            timestamp: serverTimestamp()
+        })
+
+        const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+        await uploadString(imageRef, selectedFile, "data_url").then(async snapshot => {
+            const downloadURL = await getDownloadURL(imageRef)
+
+            await updateDoc(doc(db, 'posts', docRef.id), {
+                image: downloadURL
+            })
+        })
+
+        setOpen(false)
+        setLoading(false)
+        setSelectedFile(null)
 
     }
 
@@ -125,8 +158,11 @@ function Modal() {
                                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
                                     px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 foucs:outline-none
                                     focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
-                                    disabled:cursor-not-allowed hover:disabled:bg-gray-300">
-                                        Upload Post
+                                    disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                                    onClick={uploadPost}
+                                    disabled={!selectedFile}
+                                    >
+                                        {loading ? "Uploading..." : "Upload Post"}
                                     </button>
                                 </div>
                             </div>
--- components/Posts.js
+++ components/Posts.js
@@ -1,36 +1,31 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post"
 
-const posts = [
-    {
-        id: '123',
-        username: 'user',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: "This is dope!"
-    },
-    {
-        id: '123',
-        username: 'user',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: "This is dope!"
-    },
-];
-
 function Posts() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+            setPosts(snapshot.docs)
+        })
+
+    }, [db])
     return (
         <div>
             {posts.map((post) => (
                 <Post 
                 key={post.id}
                 id={post.id}
-                username={post.username}
-                userImg={post.userImg}
-                img={post.img}
-                caption={post.caption} 
+                username={post.data().username}
+                userImg={post.data().profileImg}
+                img={post.data().image}
+                caption={post.data().caption} 
                 />
             ))} 
-        </div>
+        </div> 
     );
 }
 
