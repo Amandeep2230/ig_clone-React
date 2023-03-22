a7385f2eb39f0a6c95cd890ac202abcf82c39211
Amandeep2230 <bhalla.aman99@gmail.com>
2023-03-21 20:16:47 -0400

    comments

--- components/Post.js
+++ components/Post.js
@@ -8,8 +8,34 @@ import {
 } from "@heroicons/react/outline";
 
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Moment from "react-moment";
 
 function Post({id, username, userImg, img, caption}) {
+
+    const { data: session } = useSession()
+    const [comment, setComment] = useState("")
+    const [comments, setComments] = useState([])
+
+    useEffect(() => onSnapshot(query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')), snapshot => setComments(snapshot.docs)), [db])
+
+    const sendComment = async (e) => {
+        e.preventDefault()
+
+        const commentToSend = comment
+        setComment("")
+
+        await addDoc(collection(db, 'posts', id, 'comments'),{
+            comment: commentToSend,
+            username: session.user.name,
+            userImg: session.user.image,
+            timestamp: serverTimestamp()
+        })
+    }
+
     return (
         <div className="bg-white my-7 border rounded-sm">
 
@@ -25,14 +51,17 @@ function Post({id, username, userImg, img, caption}) {
             <img src={img} className="object-cover w-full" alt="" />
 
             {/* Buttons */}
-            <div className="flex justify-between px-4 pt-4">
+            {session && (
+                <div className="flex justify-between px-4 pt-4">
                 <div className="flex space-x-4">
                     <HeartIcon className="btn" />
                     <ChatIcon className="btn" />
                     <PaperAirplaneIcon className="btn rotate-45 h-6" />
                 </div>
                 <BookmarkIcon className="btn" />
-            </div>
+                </div>
+            )}
+            
             
 
             {/* caption */}
@@ -42,14 +71,46 @@ function Post({id, username, userImg, img, caption}) {
             </p>
 
             {/* comments */}
+            {comments.length>0 && (
+                <div
+                className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black
+                scrollbar-thin">
+                    {comments.map(comment => (
+                        <div key={comment.id} className='flex items-center space-x-2 mb-6'>
+                            <img 
+                            className="h-7 rounded-full"
+                            src={comment.data().userImg} alt="" />
+                            <p
+                            className="text-sm flex-1"
+                            ><span className="font-bold">{comment.data().name}</span>{comment.data().comment}</p>
+                            <Moment fromNow className="pr-5 text-xs">
+                                {comment.data().timestamp?.toDate()}
+                            </Moment>
+                        </div>
+                    ))}
+                </div>
+            )}
 
             {/* input box */}
-            <form className="flex items-center p-4">
+            {session && (
+                <form className="flex items-center p-4">
                 <EmojiHappyIcon className="h-7" />
-                <input type="text"
-                placeholder="Add a comment..." className="border-none flex-1 focus:ring=0 outline-none" />
-                <button className="font-semibold text-blue-400">Post</button>
+                <input 
+                type="text"
+                value={comment}
+                onChange={e => setComment(e.target.value)}
+                placeholder="Add a comment..." 
+                className="border-none flex-1 focus:ring=0 outline-none" />
+                <button
+                 onClick={sendComment}
+                type="submit" 
+                disabled={!comment.trim()} 
+                className="font-semibold text-blue-400">
+                    Post
+                </button>
             </form>
+            )}
+            
         </div>
     )
 }
