5dbd2d2c1c41065a39d702f6e4436be1d2950fbc
Amandeep2230 <bhalla.aman99@gmail.com>
2023-03-21 20:50:16 -0400

    likes

--- components/Post.js
+++ components/Post.js
@@ -8,19 +8,43 @@ import {
 } from "@heroicons/react/outline";
 
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid";
-import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Moment from "react-moment";
+import { async } from "@firebase/util";
 
 function Post({id, username, userImg, img, caption}) {
 
     const { data: session } = useSession()
     const [comment, setComment] = useState("")
     const [comments, setComments] = useState([])
+    const [likes, setLikes] = useState([])
+    const [hasLiked, setHasLiked] = useState(false)
 
     useEffect(() => onSnapshot(query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')), snapshot => setComments(snapshot.docs)), [db])
+    useEffect(() => onSnapshot(collection(db, 'posts', id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)),
+        [db, id]
+        )
+    
+        useEffect(() => 
+            setHasLiked(
+                likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+            ), [likes])
+
+    const likePost = async () => {
+        if (hasLiked) {
+            await deleteDoc(doc(db, 'posts', id, "likes", session.user.uid))
+        }
+        else {
+            await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+                username: session.user.name
+            })
+        }
+        }
+        
 
     const sendComment = async (e) => {
         e.preventDefault()
@@ -54,7 +78,13 @@ function Post({id, username, userImg, img, caption}) {
             {session && (
                 <div className="flex justify-between px-4 pt-4">
                 <div className="flex space-x-4">
-                    <HeartIcon className="btn" />
+                    {
+                        hasLiked ? (
+                            <HeartIconFilled onClick={likePost} className="btn text-red-500" />
+                        ) : (
+                            <HeartIcon onClick={likePost} className="btn" />
+                        )
+                    }
                     <ChatIcon className="btn" />
                     <PaperAirplaneIcon className="btn rotate-45 h-6" />
                 </div>
@@ -66,6 +96,12 @@ function Post({id, username, userImg, img, caption}) {
 
             {/* caption */}
             <p className="p-5 truncate">
+
+                {likes.length > 0 && (
+                    <p
+                    className="font-bold mb-1"
+                    >{likes.length} likes</p>
+                )}
                 <span className="font-bold mr-1">{username}</span>
                 {caption}
             </p>
